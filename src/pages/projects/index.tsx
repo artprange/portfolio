0a507d84ac7283be9b8d8347065faef96d2e2a8e
artprange <arthur.prange@gmail.com>
2024-08-07 19:27:42 -0300

    updates

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,9 +1,8 @@
-import { Forbidden, ImageContainer } from './styles'
+import { ImageContainer } from './styles'
 import movies from '../../assets/movies.png'
-import pizzaShop from '../../assets/pizzaShop.png'
 
 import pomodoro from '../../assets/pomodoro.png'
-import shirtShop from '../../assets/shirtShop.png'
+
 import tranceBook from '../../assets/tranceBook.png'
 import { Link } from 'react-router-dom'
 
@@ -15,21 +14,21 @@ export function Projects() {
           <img src={pomodoro} alt="" title="Pomodoro Timer" />
           <p>Pomodoro Timer</p>
         </Link>
-        <Link to="https://pizza-shop-six.vercel.app/">
+        {/* <Link to="https://pizza-shop-six.vercel.app/">
           <img src={pizzaShop} alt="" title="Pizza shop - developing" />
           <p>Pizza place dashboard - under development!</p>
-        </Link>
+        </Link> */}
         <Link to="https://we-fit-assignment.vercel.app/">
           <img src={movies} alt="" title="Movie catalog" />
           <p>Movie catalog</p>
         </Link>
 
-        <Link to="https://shirt-shop-deployed.vercel.app/">
+        {/* <Link to="https://shirt-shop-deployed.vercel.app/">
           <Forbidden>
             <img src={shirtShop} alt="" title="Shirt shop" />
             <p>Shirt shop</p>
           </Forbidden>
-        </Link>
+        </Link> */}
         <Link to="https://trancebook-deployed.vercel.app/">
           <img src={tranceBook} alt="" title="TranceBook" />
           <p>Social media coment section</p>
